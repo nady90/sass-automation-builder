a5fb6ba5cf14855a074c9fddb6569c09e78ffa8b
Abanoub <naguib.abanoub@gmail.com>
2026-02-19 22:57:07 +0200

    feat: implemented the clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -26,12 +27,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <TooltipProvider>{children}</TooltipProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={inter.variable}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <TooltipProvider>{children}</TooltipProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,14 +1,38 @@
 import Link from "next/link";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
     <nav className="flex flex-row px-4 justify-between">
       <p className="text-purple-500">LOGO</p>
-      <ul className="flex flex-row">
+      <ul className="flex flex-row gap-x-3">
         <li>home</li>
         <li>contact</li>
         <li>about</li>
       </ul>
+
+      <div>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign in</Button>
+          </SignInButton>
+          <SignUpButton>
+            <Button>Sign up</Button>
+          </SignUpButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+
       <Link href={"/dashboard"}>Dashboard</Link>
     </nav>
   );
